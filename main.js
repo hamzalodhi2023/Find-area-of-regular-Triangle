83eb473be5cb95bac476d639bcae514f567b239e
Hamza Khan Lodhi <134397582+hamzalodhi2023@users.noreply.github.com>
2024-01-16 14:24:29 +0500

    project completed

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-let body = document.querySelector("body");
-let answer = document.querySelector("#answer");
-let height = document.querySelector("#height");
-let base = document.querySelector("#base");
+let body = document.body;
+let a = document.getElementById("a");
+let b = document.getElementById("b");
+let c = document.getElementById("c");
+let answer = document.getElementById("answer");
+
+a.addEventListener("input", function () {
+  let s = (a.value + b.value + c.value) / 2;
+  let formula = s * (s - a.value) * (s - b.value) * (s - c.value);
+  let squareRoot = Math.sqrt(formula);
+  answer.value = squareRoot;
+});
+b.addEventListener("input", function () {
+  let s = (a.value + b.value + c.value) / 2;
+  let formula = s * (s - a.value) * (s - b.value) * (s - c.value);
+  let squareRoot = Math.sqrt(formula);
+  answer.value = squareRoot;
+});
+c.addEventListener("input", function () {
+  let s = (a.value + b.value + c.value) / 2;
+  let formula = s * (s - a.value) * (s - b.value) * (s - c.value);
+  let squareRoot = Math.sqrt(formula);
+  answer.value = squareRoot;
+});
 
 body.addEventListener("load", function () {
-  height.value = "";
-  base.value = "";
-});
-
-base.addEventListener("input", function () {
-  let formula = (height.value * base.value) / 2;
-  answer.value = formula;
-});
-
-height.addEventListener("input", function () {
-  let formula = (height.value * base.value) / 2;
-  answer.value = formula;
+  a.value = "";
+  b.value = "";
+  c.value = "";
+  answer.value = "";
 });
